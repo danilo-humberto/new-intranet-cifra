de853f49452915b344d93141015edc628d51177e
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-30 13:16:35 -0300

    fix: alterando o layout para a sidebar aparecer em determinadas rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Main from "./pages/Main";
+import Portais from "./pages/Portais";
+import EletronicDiary from "./pages/EletronicDiary";
+import ManagementPortals from "./pages/admin/ManagementPortals";
+import ManagementUsers from "./pages/admin/ManagementUsers";
 
 function App() {
   return (
@@ -9,7 +13,19 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Main />} />
+        <Route
+          path="/*"
+          element={
+            <Main>
+              <Routes>
+                <Route path="portals" element={<Portais />} />
+                <Route path="eletronicDiary" element={<EletronicDiary />} />
+                <Route path="admin/portals" element={<ManagementPortals />} />
+                <Route path="admin/users" element={<ManagementUsers />} />
+              </Routes>
+            </Main>
+          }
+        />
       </Routes>
     </>
   );
