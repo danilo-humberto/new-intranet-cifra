0464a56f992d415e152b98927b2e346236519ff3
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-30 13:17:06 -0300

    fix: Alterando o título e adicionando o controle do estado global da sidebar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,26 @@
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import MenuMobile from "./MenuMobile";
+import { Button } from "../ui/button";
+import { ArrowBigRightDash } from "lucide-react";
+import { useSidebarState } from "@/stores/SidebarState";
 
 const Header = () => {
+  const { toggle, open } = useSidebarState();
   return (
-    <header className="border-b p-4 flex justify-between">
+    <header className="shadow-sm p-4 flex justify-between">
       <div className="flex items-center gap-2">
         <div className="md:hidden">
           <MenuMobile />
         </div>
-        <p className="text-xl font-bold">Dashboard</p>
+        <Button variant="outline" onClick={toggle}>
+          <ArrowBigRightDash
+            size={16}
+            className={`${open ? "rotate-180" : ""} text-muted-foreground`}
+          />
+        </Button>
+        <p className="text-xl font-bold capitalize">Intranet Cifra</p>
       </div>
-      <Avatar>
+      <Avatar className="select-none">
         <AvatarFallback>US</AvatarFallback>
       </Avatar>
     </header>
