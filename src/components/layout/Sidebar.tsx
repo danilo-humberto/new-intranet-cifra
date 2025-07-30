cec60f163427aa5f83287f29a7f02d283a8664d7
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-30 13:17:22 -0300

    feat: configurando a sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,34 +1,50 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import { Separator } from "../ui/separator";
-import { CalendarSearch, Columns4, Settings, Users } from "lucide-react";
+import {
+  CalendarSearch,
+  Columns4,
+  LogOut,
+  Settings,
+  Users,
+} from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { useSidebarState } from "@/stores/SidebarState";
 
-const Sidebar = ({
-  open,
-  setSidebarOpen,
-}: {
-  open: boolean;
-  setSidebarOpen: any;
-}) => {
-  const isActive = true;
+const Sidebar = ({ isAdmin }: { isAdmin: boolean }) => {
+  const { pathname } = useLocation();
+
+  const { open } = useSidebarState();
+
+  const checkIsActive = (path: string) => {
+    return pathname === path || pathname.startsWith(path + "/");
+  };
+
   return (
     <div
-      className={`hidden md:block shadow-xl bg-background h-screen fixed top-0 left-0 transition-all duration-300 ${
-        open ? "w-72" : "w-16"
+      className={`hidden md:block shadow-xl cursor-pointer relative bg-background min-h-screen transition-all duration-300 ${
+        open ? "w-80" : "w-16"
       }`}
-      onMouseEnter={() => setSidebarOpen(true)}
-      onMouseLeave={() => setSidebarOpen(false)}
     >
       <figure className="p-3">
-        <img src="/logo-cifra-nova.png" alt="" width={120} />
+        <img
+          src={"/logo-cifra-nova.png"}
+          alt=""
+          className="h-10 object-contain transition-all duration-300"
+        />
       </figure>
+
       <Separator />
       <div className="p-3">
         <div className="flex items-center gap-3 px-1">
           <Avatar>
             <AvatarFallback>US</AvatarFallback>
           </Avatar>
-          <div className="text-sm">
+          <div
+            className={`whitespace-pre duration-300 text-sm ${
+              !open && "opacity-0 translate-x-24 overflow-hidden"
+            } `}
+          >
             <p>Nome do Usuário</p>
             <span className="text-muted-foreground text-xs">Cargo</span>
           </div>
@@ -37,49 +53,164 @@ const Sidebar = ({
       <Separator />
       <div className="p-3">
         <div className="flex flex-col gap-2">
-          <Link
-            to={"/portais"}
-            className={`flex items-center gap-4 p-3 hover:bg-gold-yellow hover:text-background transition-colors duration-150 ${
-              isActive ? "bg-gold-yellow text-background" : ""
-            } rounded text-sm`}
-          >
-            <Columns4 size={16} />
-            <span>Portais</span>
-          </Link>
-          <Link
-            to={"/portais"}
-            className={`flex items-center gap-4 p-3 hover:bg-gold-yellow hover:text-background transition-colors duration-150 ${
-              !isActive ? "bg-gold-yellow text-background" : ""
-            } rounded text-sm`}
-          >
-            <CalendarSearch size={16} />
-            <span>Agenda Eletrônica</span>
-          </Link>
+          <Tooltip delayDuration={200}>
+            <TooltipTrigger asChild>
+              <Link
+                to={"/portals"}
+                className={`flex items-center gap-4 p-3 transition-colors duration-150 ${
+                  checkIsActive("/portals")
+                    ? "bg-gold-yellow text-background"
+                    : "hover:bg-muted"
+                } rounded text-sm`}
+              >
+                <div>
+                  <Columns4 size={16} />
+                </div>
+                <span
+                  className={`whitespace-pre duration-300 ${
+                    !open && "opacity-0 translate-x-24 overflow-hidden"
+                  } `}
+                >
+                  Portais
+                </span>
+              </Link>
+            </TooltipTrigger>
+            {!open && (
+              <TooltipContent side="right">
+                <p>Portais</p>
+              </TooltipContent>
+            )}
+          </Tooltip>
+          <Tooltip delayDuration={200}>
+            <TooltipTrigger asChild>
+              <Link
+                to={"/eletronicDiary"}
+                className={`flex items-center gap-4 p-3 transition-colors duration-150 ${
+                  checkIsActive("/eletronicDiary")
+                    ? "bg-gold-yellow text-background"
+                    : "hover:bg-muted"
+                } rounded text-sm`}
+              >
+                <div>
+                  <CalendarSearch size={16} />
+                </div>
+                <span
+                  className={`whitespace-pre duration-300 ${
+                    !open && "opacity-0 translate-x-24 overflow-hidden"
+                  } `}
+                >
+                  Agenda Eletrônica
+                </span>
+              </Link>
+            </TooltipTrigger>
+            {!open && (
+              <TooltipContent side="right">
+                <p>Agenda Eletrônica</p>
+              </TooltipContent>
+            )}
+          </Tooltip>
         </div>
       </div>
-      <p className="text-sm text-muted-foreground uppercase px-3">
-        Administração
-      </p>
-      <div className="p-3">
-        <div className="flex flex-col gap-2">
-          <Link
-            to={"/admin/portals"}
-            className={`flex items-center gap-4 p-3 hover:bg-gold-yellow hover:text-background transition-colors duration-150 ${
-              !isActive ? "bg-gold-yellow text-background" : ""
-            } rounded text-sm`}
-          >
-            <Settings size={16} />
-            <span>Gerenciar Portais</span>
-          </Link>
-          <Link
-            to={"/admin/users"}
-            className={`flex items-center gap-4 p-3 hover:bg-gold-yellow hover:text-background transition-colors duration-150 ${
-              !isActive ? "bg-gold-yellow text-background" : ""
-            } rounded text-sm`}
-          >
-            <Users size={16} />
-            <span>Gerenciar Usuários</span>
-          </Link>
+      {isAdmin && (
+        <>
+          {open ? (
+            <p className="text-xs text-muted-foreground uppercase px-3">
+              Administração
+            </p>
+          ) : (
+            <p className="text-xs text-muted-foreground uppercase px-3">
+              Admin
+            </p>
+          )}
+          <div className="p-3">
+            <div className="flex flex-col gap-2">
+              <Tooltip delayDuration={200}>
+                <TooltipTrigger asChild>
+                  <Link
+                    to={"/admin/portals"}
+                    className={`flex items-center gap-4 p-3 transition-colors duration-150 ${
+                      checkIsActive("/admin/portals")
+                        ? "bg-gold-yellow text-background"
+                        : "hover:bg-muted"
+                    } rounded text-sm`}
+                  >
+                    <div>
+                      <Settings size={16} />
+                    </div>
+                    <span
+                      className={`whitespace-pre duration-300 ${
+                        !open && "opacity-0 translate-x-24 overflow-hidden"
+                      } `}
+                    >
+                      Gerenciar Portais
+                    </span>
+                  </Link>
+                </TooltipTrigger>
+                {!open && (
+                  <TooltipContent side="right">
+                    <p>Gerenciar Portais</p>
+                  </TooltipContent>
+                )}
+              </Tooltip>
+              <Tooltip delayDuration={200}>
+                <TooltipTrigger asChild>
+                  <Link
+                    to={"/admin/users"}
+                    className={`flex items-center gap-4 p-3 transition-colors duration-150 ${
+                      checkIsActive("/admin/users")
+                        ? "bg-gold-yellow text-background"
+                        : "hover:bg-muted"
+                    } rounded text-sm`}
+                  >
+                    <div>
+                      <Users size={16} />
+                    </div>
+                    <span
+                      className={`whitespace-pre duration-300 ${
+                        !open && "opacity-0 translate-x-24 overflow-hidden"
+                      } `}
+                    >
+                      Gerenciar Usuários
+                    </span>
+                  </Link>
+                </TooltipTrigger>
+                {!open && (
+                  <TooltipContent side="right">
+                    <p>Gerenciar Usuários</p>
+                  </TooltipContent>
+                )}
+              </Tooltip>
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="absolute bottom-0 border-t w-full">
+        <div className="p-3">
+          <Tooltip delayDuration={200}>
+            <TooltipTrigger asChild>
+              <Link
+                to={"/login"}
+                className={`flex items-center gap-4 p-3 hover:bg-muted transition-colors duration-150 rounded text-sm`}
+              >
+                <div>
+                  <LogOut size={16} className="text-red-500" />
+                </div>
+                <span
+                  className={`whitespace-pre duration-300 text-red-500 ${
+                    !open && "opacity-0 translate-x-24 overflow-hidden"
+                  } `}
+                >
+                  Sair
+                </span>
+              </Link>
+            </TooltipTrigger>
+            {!open && (
+              <TooltipContent side="right">
+                <p>Sair</p>
+              </TooltipContent>
+            )}
+          </Tooltip>
         </div>
       </div>
     </div>
