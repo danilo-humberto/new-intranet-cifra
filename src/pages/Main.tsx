d68e5e36269523ca64de42813265bceff6901cbb
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-30 13:18:13 -0300

    fix: configurando o layout

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,20 +1,17 @@
 import Header from "@/components/layout/Header";
 import Sidebar from "@/components/layout/Sidebar";
-import { useState } from "react";
 
-const Main = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+const Main = ({ children }: { children: React.ReactNode }) => {
+  const isAdmin = true;
   return (
     <>
       <div className="flex">
-        <Sidebar open={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <Sidebar isAdmin={isAdmin} />
         <div
-          className={`flex flex-col w-full transition-all duration-300 ${
-            sidebarOpen ? "pl-72" : "pl-16"
-          }`}
+          className={`flex flex-col w-full transition-all duration-300 max-h-screen overflow-y-scroll`}
         >
           <Header />
-          <main>teste</main>
+          <main>{children}</main>
         </div>
       </div>
     </>
