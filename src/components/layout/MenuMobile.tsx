74467538c719943c712d14d0ed1dadd42fca4787
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-30 16:54:46 -0300

    fix: alterando a cor do texto de saída

--- src/components/layout/MenuMobile.tsx
+++ src/components/layout/MenuMobile.tsx
@@ -3,24 +3,26 @@ import { Button } from "../ui/button";
 import { Sheet, SheetContent, SheetFooter, SheetTrigger } from "../ui/sheet";
 import SheetContentHeader from "../SheetContentHeader";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const MenuMobile = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" onClick={() => setOpen(true)}>
           <Menu />
         </Button>
       </SheetTrigger>
       <SheetContent side="left">
-        <SheetContentHeader />
+        <SheetContentHeader setOpen={setOpen} />
         <SheetFooter className="border-t">
           <Link
-            to={"/login"}
-            className="flex items-center gap-4 p-3 hover:bg-gold-yellow hover:text-background transition-colors duration-150 rounded text-sm"
+            to={"/"}
+            className="flex items-center gap-4 p-3 hover:bg-muted transition-colors duration-150 rounded text-sm"
           >
-            <LogOut size={16} />
-            <span>Sair</span>
+            <LogOut size={16} className="text-red-500" />
+            <span className="text-red-500">Sair</span>
           </Link>
         </SheetFooter>
       </SheetContent>
