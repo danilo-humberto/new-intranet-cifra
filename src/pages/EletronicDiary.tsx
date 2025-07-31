ad5e2d00f795d5ca1a9b1e88cc9d84f8e9b8f9a3
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-31 16:52:55 -0300

    fix: alterando o título para colocar icone antes do texto

--- src/pages/EletronicDiary.tsx
+++ src/pages/EletronicDiary.tsx
@@ -5,7 +5,7 @@ const EletronicDiary = () => {
   return (
     <section className="py-4 px-5">
       <div className="flex flex-col gap-4 justify-between lg:flex-row">
-        <h1 className="text-2xl font-semibold">Agenda Eletrônica</h1>
+        <h1 className="text-2xl font-semibold">🗓️ Agenda Eletrônica</h1>
         <div className="flex gap-2">
           <div className="flex flex-1 h-10 items-center gap-2 border border-input rounded-sm p-2 md:text-sm w-72 focus-within:border-gold-yellow">
             <Search size={20} className="text-muted-foreground" />
--- src/pages/Portais.tsx
+++ src/pages/Portais.tsx
@@ -5,7 +5,7 @@ const Portais = () => {
   return (
     <section className="py-4 px-5">
       <div className="flex flex-col gap-4 justify-between lg:flex-row">
-        <h1 className="text-2xl font-semibold">Portais Disponíveis</h1>
+        <h1 className="text-2xl font-semibold">📋 Portais Disponíveis</h1>
         <div className="flex gap-2">
           <div className="flex flex-1 h-10 items-center gap-2 border border-input rounded-sm p-2 md:text-sm w-72 focus-within:border-gold-yellow">
             <Search size={20} className="text-muted-foreground" />
