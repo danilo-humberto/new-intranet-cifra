79b4f694b9fa0e428ab35c68108da0ffd3c6d651
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-31 16:53:23 -0300

    feat: adicionando o componente gerenciado apenas por admin

--- src/pages/admin/ManagementPortals.tsx
+++ src/pages/admin/ManagementPortals.tsx
@@ -1,5 +1,29 @@
+import AddPortalsDialog from "@/components/dialogs/PortalsDialog";
+import ManagementePortalsTable from "@/components/ManagementPortalsTable";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+
 const ManagementPortals = () => {
-  return <div>ManagementPortals</div>;
+  return (
+    <section className="py-4 px-5">
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl font-semibold">📋 Portais</h2>
+        <Dialog>
+          <form>
+            <DialogTrigger>
+              <Button variant="default" size={"lg"} type="button">
+                <Plus />
+                Novo Portal
+              </Button>
+            </DialogTrigger>
+            <AddPortalsDialog />
+          </form>
+        </Dialog>
+      </div>
+      <ManagementePortalsTable />
+    </section>
+  );
 };
 
 export default ManagementPortals;
--- src/pages/admin/ManagementUsers.tsx
+++ src/pages/admin/ManagementUsers.tsx
@@ -1,5 +1,29 @@
+import UsersDialog from "@/components/dialogs/UsersDialog";
+import ManagementUsersTable from "@/components/ManagementUsersTable";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+
 const ManagementUsers = () => {
-  return <div>ManagementUsers</div>;
+  return (
+    <section className="py-4 px-5">
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl font-semibold">👤 Usuários</h2>
+        <Dialog>
+          <form>
+            <DialogTrigger>
+              <Button variant="default" size={"lg"} type="button">
+                <Plus />
+                Novo Usuário
+              </Button>
+            </DialogTrigger>
+            <UsersDialog />
+          </form>
+        </Dialog>
+      </div>
+      <ManagementUsersTable />
+    </section>
+  );
 };
 
 export default ManagementUsers;
