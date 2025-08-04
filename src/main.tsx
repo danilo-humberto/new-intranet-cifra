125dfdb98f1292b7ef63bd9beae7577051b84994
Danilo Humberto <danilohumberto12@hotmail.com>
2025-08-04 16:33:24 -0300

    feat: adicionando o toaster

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <Toaster position="bottom-right" />
         <App />
       </BrowserRouter>
     </QueryClientProvider>
