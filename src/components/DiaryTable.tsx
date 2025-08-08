c1b4e8a124ac0f4fabf7b9b3e74705073f89f8e2
Danilo Humberto <danilohumberto12@hotmail.com>
2025-08-08 16:03:32 -0300

    feat: controlando os dados da tabela dependendo da role do usuario

--- src/components/DiaryTable.tsx
+++ src/components/DiaryTable.tsx
@@ -7,12 +7,14 @@ import {
   TableHeader,
   TableRow,
 } from "./ui/table";
+import { getStorageItem } from "@/utils/Storage";
 
 interface DiaryTableProps {
   users: User[] | [];
 }
 
 const DiaryTable = ({ users }: DiaryTableProps) => {
+  const storage = getStorageItem("user");
   return (
     <Table className="mt-4">
       <TableHeader>
@@ -22,21 +24,25 @@ const DiaryTable = ({ users }: DiaryTableProps) => {
           <TableHead className="text-muted-foreground">
             Telefone Funcional
           </TableHead>
-          <TableHead className="text-muted-foreground">
-            Telefone Pessoal
-          </TableHead>
+          {storage?.roles[0].code === "1" && (
+            <TableHead className="text-muted-foreground">
+              Telefone Pessoal
+            </TableHead>
+          )}
           <TableHead className="text-muted-foreground">Cargo</TableHead>
           <TableHead className="text-muted-foreground">Estado</TableHead>
           <TableHead className="text-muted-foreground">Lotação</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {users.map((user) => (
+        {users?.map((user) => (
           <TableRow key={user._id}>
             <TableCell className="py-6">{user.name}</TableCell>
             <TableCell>{user.email}</TableCell>
             <TableCell>{user.number}</TableCell>
-            <TableCell>{user.personalNumber}</TableCell>
+            {storage?.roles[0].code === "1" && (
+              <TableCell>{user.personalNumber}</TableCell>
+            )}
             <TableCell>{user.function}</TableCell>
             <TableCell>{user.state}</TableCell>
             <TableCell>{user.lotation}</TableCell>
