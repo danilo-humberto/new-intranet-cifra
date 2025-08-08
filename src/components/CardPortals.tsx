669020210eda5fb0b068d2ba92f840fa76c7a047
Danilo Humberto <danilohumberto12@hotmail.com>
2025-08-08 16:03:12 -0300

    fix: alterando para receber os dados por props

--- src/components/CardPortals.tsx
+++ src/components/CardPortals.tsx
@@ -1,30 +1,29 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import type { Portals } from "@/types/Portals";
 
 interface CardPortalsProps {
-  id?: string;
-  title: string;
-  description: string;
-  imageUrl: string;
+  portal: Portals;
 }
 
-const CardPortals = ({
-  title,
-  description,
-  imageUrl,
-  id,
-}: CardPortalsProps) => {
+const CardPortals = ({ portal }: CardPortalsProps) => {
   return (
-    <div className="border border-input rounded-sm p-4 flex flex-col gap-3 transition-all duration-150 hover:shadow">
+    <div className="border border-input rounded-sm p-4 flex flex-col justify-between gap-3 transition-all duration-150 hover:shadow">
       <div className="flex items-center">
         <figure className="w-12 h-12 shrink-0 rounded-sm overflow-hidden">
-          <img src={imageUrl} alt="" className="object-contain w-full h-full" />
+          <img
+            src={portal.image}
+            alt=""
+            className="object-contain w-full h-full"
+          />
         </figure>
-        <h2 className="text-lg font-semibold truncate pl-2">{title}</h2>
+        <h2 className="text-lg font-semibold truncate pl-2">{portal.name}</h2>
       </div>
-      <p className="text-muted-foreground text-sm ">{description}</p>
-      <Link to={`/portals/${id}`}>
+      <p className="text-muted-foreground text-sm ">
+        {portal.shortDescription}
+      </p>
+      <Link to={`/portals/${portal._id}`}>
         <Button variant="outline" className="flex-1 w-full group" size="lg">
           Ver Detalhes
           <ArrowRight
