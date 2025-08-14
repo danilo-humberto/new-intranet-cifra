2856a0d3bc3039fd560207eeb802ef5632516766
Danilo <danilohumberto12@hotmail.com>
2025-08-14 16:20:30 -0300

    feat: adicionando interceptors para validar o token

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,26 @@
+import { useSessionAlert } from "@/stores/useSessionAlert";
+import { getStorageItem, removeStorageItem } from "@/utils/Storage";
 import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
+api.interceptors.request.use((config) => {
+  const token = getStorageItem("user")?.accessToken;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      removeStorageItem("user");
+      useSessionAlert.getState().setOpen(true);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
