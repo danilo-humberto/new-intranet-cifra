3840106f2afeefd3372a2acce44569bca862b6fc
Danilo <danilohumberto12@hotmail.com>
2025-08-14 16:20:49 -0300

    fix: tirando as autorizações

--- src/api/Portals.endpoints.ts
+++ src/api/Portals.endpoints.ts
@@ -1,73 +1,46 @@
 import type { Portals } from "@/types/Portals";
 import api from "./axios";
-import { useSessionAlert } from "@/stores/useSessionAlert";
-import { getStorageItem } from "@/utils/Storage";
 
 export const getPortals = async () => {
-  const token = getStorageItem("user")?.accessToken;
-  try {
-    const { data } = await api.get("/portals", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
+  const { data } = await api.get("/portals", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    return data;
-  } catch (error: any) {
-    if (error.response?.status === 401) {
-      useSessionAlert.getState().setOpen(true);
-    }
-    throw error;
-  }
+  return data;
 };
 
 export const getPortalsById = async (portalId: string) => {
-  const token = getStorageItem("user")?.accessToken;
+  const { data } = await api.get(`/portals/${portalId}?_embed=inscriptions`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-  try {
-    const { data } = await api.get(`/portals/${portalId}?_embed=inscriptions`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    return data;
-  } catch (error: any) {
-    if (error.response?.status === 401) {
-      useSessionAlert.getState().setOpen(true);
-    }
-    throw error;
-  }
+  return data;
 };
 
 export const deletePortal = async (portalId: string) => {
-  const token = getStorageItem("user")?.accessToken;
   await api.delete(`/portals/${portalId}`, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
     },
   });
 };
 
 export const createPortal = async (portal: Portals) => {
-  const token = getStorageItem("user")?.accessToken;
   await api.post("/portals/auth/register", portal, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
     },
   });
 };
 
 export const updatePortal = async (portalId: string, portal: Portals) => {
-  const token = getStorageItem("user")?.accessToken;
   await api.put(`/portals/${portalId}`, portal, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
     },
   });
 };
--- src/api/Users.endpoints.ts
+++ src/api/Users.endpoints.ts
@@ -1,7 +1,6 @@
-import { getStorageItem, setStorageItem } from "@/utils/Storage";
+import { setStorageItem } from "@/utils/Storage";
 import api from "./axios";
 import type { UserPayload } from "@/types/User";
-import { useSessionAlert } from "@/stores/useSessionAlert";
 
 export const createUser = async (user: UserPayload) => {
   const { data } = await api.post(`/auth/register`, user, {
@@ -13,47 +12,35 @@ export const createUser = async (user: UserPayload) => {
 };
 
 export const getUsers = async () => {
-  const token = getStorageItem("user")?.accessToken;
-  const response = await api.get("/users", {
+  const { data } = await api.get("/users", {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
     },
   });
-  if (response.status === 401) {
-    useSessionAlert.getState().setOpen(true);
-    return;
-  }
-  return response.data;
+  return data;
 };
 
 export const deleteUser = async (id: string) => {
-  const token = getStorageItem("user")?.accessToken;
   await api.delete(`/user/${id}`, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
     },
   });
 };
 
 export const getUserById = async (id: string) => {
-  const token = getStorageItem("user")?.accessToken;
   const { data } = await api.get(`/user/${id}`, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
     },
   });
   return data;
 };
 
 export const updateUser = async (id: string, user: UserPayload) => {
-  const token = getStorageItem("user")?.accessToken;
   await api.put(`/user/${id}`, user, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
     },
   });
 };
