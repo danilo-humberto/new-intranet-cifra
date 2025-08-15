ae78dece9a28430ad3b69abed453db4fc9b053eb
Danilo <danilohumberto12@hotmail.com>
2025-08-15 18:34:09 -0300

    feat: configurando para quando clicar em editar perfil, ele fechar o menu

--- src/components/layout/FooterSidebarMobile.tsx
+++ src/components/layout/FooterSidebarMobile.tsx
@@ -2,8 +2,13 @@ import { ArrowUpDown, File, LogOut, SquarePen } from "lucide-react";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Link } from "react-router-dom";
+import type { Dispatch, SetStateAction } from "react";
 
-const FooterSidebar = () => {
+interface FooterSidebarProps {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const FooterSidebar = ({ setOpen }: FooterSidebarProps) => {
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -21,30 +26,36 @@ const FooterSidebar = () => {
         </div>
       </PopoverTrigger>
       <PopoverContent className="p-2">
-        <div className="p-2 flex flex-col gap-2 text-sm">
-          <Link
-            to={"/profile"}
-            className="flex items-center gap-2 transition-all duration-150 hover:bg-muted rounded-sm p-2"
-          >
-            <SquarePen size={16} />
-            <span>Editar Perfil</span>
-          </Link>
-          <Link
-            to={"codigo-conduta-cifra.pdf"}
-            download={true}
-            className="flex items-center gap-2 transition-all duration-150 hover:bg-muted rounded-sm p-2"
-          >
-            <File size={16} />
-            <span>Código de Conduta</span>
-          </Link>
-          <Link
-            to={"/"}
-            className="flex items-center gap-2 transition-all duration-150 hover:bg-muted rounded-sm p-2"
-          >
-            <LogOut size={16} className="text-red-500" />
-            <span className="text-red-500">Sair</span>
-          </Link>
-        </div>
+        <ul className="p-2 flex flex-col gap-2 text-sm">
+          <li onClick={() => setOpen(false)}>
+            <Link
+              to={"/profile"}
+              className="flex items-center gap-2 transition-all duration-150 hover:bg-muted rounded-sm p-2"
+            >
+              <SquarePen size={16} />
+              <span>Editar Perfil</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={"codigo-conduta-cifra.pdf"}
+              download={true}
+              className="flex items-center gap-2 transition-all duration-150 hover:bg-muted rounded-sm p-2"
+            >
+              <File size={16} />
+              <span>Código de Conduta</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={"/"}
+              className="flex items-center gap-2 transition-all duration-150 hover:bg-muted rounded-sm p-2"
+            >
+              <LogOut size={16} className="text-red-500" />
+              <span className="text-red-500">Sair</span>
+            </Link>
+          </li>
+        </ul>
       </PopoverContent>
     </Popover>
   );
